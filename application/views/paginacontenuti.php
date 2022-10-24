c8507b2d476c5936f3cd7d257782c69977c97c76
Emanuele Patruno <44495714+FirstGameLife@users.noreply.github.com>
2022-10-24 17:22:50 +0200

    Titolo della casella selezionata + indice dinamico

--- application/views/paginacontenuti.php
+++ application/views/paginacontenuti.php
@@ -2,21 +2,9 @@
   <head>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <title>Mostra dati</title>
-    <style>
-     
-    </style>
   </head>
  
   <body>
-  <script>
-        var base_url = 'http://localhost/example/';
-
-        function change() {
-                            location.href = base_url + 'crud/paginacontenuti?idsp=9';
-                            //console.log('' + "ciao");
-                            //$("#cambio").html('');
-                        }
-    </script>
     <?php   
         $nome = "";
         if (!$this->input->get('idsp')) {
@@ -27,30 +15,51 @@
             $idspec = $this->input->get('idsp');
             $data = $this->Crud_model->get_singolo_valore($idspec);
             $nome = $data['titolo'];
-            var_dump($idspec);
         }
-        
         ?>
-
-    <div>
-        <h2 id="copia"></h2>
-    </div>
 
     <button onclick="change()">ciao</button>
     <?php
     ?>
 
-    <select class="js-example-basic-single" name="state" >
-    <?php
-    
-    foreach($pagine as $dati){
-    ?>
-        <option =""><?= $dati['titolo'] ?></option>
-    <?php 
-    }
-    ?>
-    
+    <select id="valueSelect" class="js-example-basic-single" name="state" >
+        <?php
         
+        foreach($pagine as $dati){
+        ?>
+            <option><?= $dati['titolo'] ?></option>
+        <?php 
+        }
+        ?>    
     </select>
+
+    <div>
+        <h2 id="result"></h2>
+    </div>
+
+    <script>
+        const selectElem = document.getElementById('valueSelect');
+        const resultElem = document.getElementById('result');
+        var indice = "";
+
+        window.onload=function(){
+
+            localStorage.setItem("result", $('#resultElem').val());
+
+            selectElem.addEventListener("change", () =>{
+            var indice = selectElem.selectedIndex;
+            location.href = base_url + 'crud/paginacontenuti?idsp=' + indice;
+            var index = $("#valueSelect option:selected").text();
+            resultElem.textContent = `${index}`;
+            location.href = base_url + 'crud/paginacontenuti?idsp=' + indice;
+            })
+        }
+        var base_url = 'http://localhost/example/';
+
+        function change() {
+                        location.href = base_url + 'crud/paginacontenuti?idsp=' + indice;
+                    }
+                    console.log(indice);
+    </script>
     </body>
 </html>
